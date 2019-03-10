dbafe731927e300959730aee51347d95c64da251
mlazaric <marko.lazaric@gmail.com>
2019-03-10 14:50:55 +0100

    Write javadoc for Factorial and PromptHelper

--- hw01-0036505387/src/main/java/hr/fer/zemris/java/hw01/Factorial.java
+++ hw01-0036505387/src/main/java/hr/fer/zemris/java/hw01/Factorial.java
@@ -4,8 +4,24 @@ import java.util.Scanner;
 
 import hr.fer.zemris.java.hw01.PromptHelper.EndOfInputReachedException;
 
+/**
+ * A program for calculating the factorial of a number.
+ * 
+ * @author Marko Lazarić
+ *
+ */
 public class Factorial {
 
+	/**
+	 *   <p>Continually prompts the user for an integer and calculates the factorial
+	 * of the given number if it is within the accepted range. Exits when the user 
+	 * inputs "kraj".</p>
+	 *  
+	 *   <p>If the user inputs an invalid number or an integer outside the accepted 
+	 * range, a relevant message is printed to the console.</p>
+	 * 
+	 * @param args the arguments are ignored
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -28,6 +44,16 @@ public class Factorial {
 		sc.close();
 	}
 	
+	/**
+	 *   <p>Calculates the factorial of the argument if it is within the accepted
+	 * range.</p>
+	 * 
+	 * @param number number for which to calculate the factorial
+	 * @return factorial of the number
+	 * 
+	 * @throws IllegalArgumentException if the number is not within the accepted
+	 *                                  range [3, 20]
+	 */
 	public static long factorial(int number) {
 		if (number < 3 || number > 20) {
 			String message = String.format("'%d' nije broj u dozvoljenom rasponu.", number);
--- hw01-0036505387/src/main/java/hr/fer/zemris/java/hw01/PromptHelper.java
+++ hw01-0036505387/src/main/java/hr/fer/zemris/java/hw01/PromptHelper.java
@@ -2,8 +2,28 @@ package hr.fer.zemris.java.hw01;
 
 import java.util.Scanner;
 
+/**
+ * A helper class for prompting the user for a specific type of value.
+ * 
+ * @author Marko Lazarić
+ *
+ */
 public class PromptHelper {
 	
+	/**
+	 *   <p>Continually prompts the user for a valid integer. If the user has entered
+     * a valid integer, the integer is returned. Otherwise a helpful message is
+     * printed to the console and the user is prompted again.</p>
+     * 
+     *   <p>If the user enters "kraj", an exception is thrown to indicated that the
+     * end of the input stream has been reached.</p>
+	 *   
+	 * @param sc instance of {@link Scanner}, used for input
+	 * @param name name of the attribute being prompted
+	 * @return the first valid integer entered by the user
+	 * 
+	 * @throws EndOfInputReachedException when the user inputs "kraj".
+	 */
 	public static int promptInteger(Scanner sc, String name) {
 		while (true) {
 			System.out.format("Unesite %s > ", name);
@@ -22,28 +42,38 @@ public class PromptHelper {
 		}
 	}
 	
-	public static double promptDouble(Scanner sc, String name) {
+	public static double promptPositiveDouble(Scanner sc, String name) {
 		while (true) {
 			System.out.format("Unesite %s > ", name);
 			
-			if (sc.hasNextDouble()) {
-				return sc.nextDouble();
-			}
-			
 			String input = sc.next();
 			
-			if (input.toLowerCase().equals("kraj")) {
-				throw new EndOfInputReachedException("Doviđenja");
+			try {
+				double value = Double.parseDouble(input);
+				
+				if (value < 0) {
+					System.out.println("Unijeli ste negativnu vrijednost.");
+				}
+			} catch (NumberFormatException e) {
+				if (input.toLowerCase().equals("kraj")) {
+					throw new EndOfInputReachedException("Doviđenja");
+				}
+				
+				System.out.format("'%s' se ne može protumačiti kao broj.%n", input);
 			}
-			
-			System.out.format("'%s' se ne može protumačiti kao broj.%n", input);
 		}
 	}
 	
+	/**
+	 * An exception used to indicate the user wants to close the prompt.
+	 * 
+	 * @author Marko Lazarić
+	 *
+	 */
 	@SuppressWarnings("serial")
 	public static class EndOfInputReachedException extends RuntimeException {
-		public EndOfInputReachedException(String string) {
-			super(string);
+		public EndOfInputReachedException(String message) {
+			super(message);
 		}
 	}
 }
